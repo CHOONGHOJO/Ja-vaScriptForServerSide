c82dc8eb789b1f88541b9db0a33cda07228ab84e
CHOONGHOJO <46162087+CHOONGHOJO@users.noreply.github.com>
2020-11-17 16:51:03 -0500

    Nov/17/2020 450pm
    Dynamic: Added html code

--- 17app.js
+++ 17app.js
@@ -4,6 +4,31 @@ var app = express();
 app.use(express.static('public'));
 app.get('/', function(req, res){        //get:routing, router 길을 찾는다라는 뜻
     res.send('Welcome home page!!');
+}); 
+
+app.get('/dynamic', function(req, res){
+    var lis ='';
+    for (var i=0; i < 5; i++){
+        lis = lis + '<li>coding</li>';
+    }
+    var time=Date();  //Eastern Standard Time
+    var output = `   
+    <!DOCTYPE html>
+<head>
+    <meta charset="UTF-8">
+    <title>Dynamic</title>
+</head>
+<body>
+   Hello, Dynamic <br>
+   <ul>
+        ${lis}
+    </ul>
+    ${time}
+
+</body>
+</html>
+    `
+    res.send(output);
 });
 
 app.get('/route', function(req, res){
