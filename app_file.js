02a5e1e9533ea55bab4e22defb77fdfb3ac28101
CHOONGHOJO <46162087+CHOONGHOJO@users.noreply.github.com>
2020-11-20 18:29:55 -0500

    Nov/20/2020 630pm
    Add OrientDB

--- app_file.js
+++ app_file.js
@@ -1,14 +1,39 @@
 var express = require('express');
-var app = express();
+// file upload
+var multer = require('multer');
+var upload = multer({ dest: 'uploads/' })
+var upload = multer({ storage: _storage })
 
+var _storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, 'uploads/')
+    },
+    filename: function (req, file, cb) {
+      const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
+      cb(null, file.originalname)
+    //   cb(null, file.originalname + '-' + uniqueSuffix)
+    }
+  })
+var upload = multer({ storage: _storage })
+
+var fs = require('fs');
+
+var app = express();
 app.use(express.json()) // for parsing application/json
 app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
-fs = require('fs');
 app.locals.pretty = true;
 
 //template directory, file 선언
 app.set('views', './views_file');
 app.set('view engine', 'jade');
+
+app.get('/upload', function(req, res){
+    res.render('upload');
+});
+app.post('/upload', upload.single('userfile'), function(req, res){
+    console.log(req.file);
+    res.send('Uploaded: '+req.file.filename);
+});
 
 //사용자요청을 적당한 controller와 연결해 주는 routing작업
 app.get('/topic/new', function(req, res){
