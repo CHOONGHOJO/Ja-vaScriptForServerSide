6a359326d2a3cd03d36b1a94c2ee4636cae3db1b
CHOONGHOJO <cjoe731@hotmail.com>
2020-11-27 13:53:38 -0500

    Nov/27/2020 153pm

--- app_passport_file.js
+++ app_passport_file.js
@@ -5,6 +5,7 @@ var bodyParser = require('body-parser');
 var bkfd2Password = require("pbkdf2-password");
 var passport = require('passport')
 var LocalStrategy = require('passport-local').Strategy;
+var FacebookStrategy = require('passport-facebook').Strategy;
 var hasher = bkfd2Password();
 
 var app = express();
@@ -33,7 +34,8 @@ app.get('/auth/logout', function(req, res){
 });
 //==============================
 app.get('/welcome', function(req, res){
-  if (req.user && req.user.displayName) {
+    if (typeof req.user != "undefined" && typeof req.user.displayName != "undefined") {
+    // if (req.user && req.user.displayName) {
     res.send(`
       <h1>Hello, ${req.user.displayName}</h1>
       <a href="/auth/logout">logout</a>
@@ -51,17 +53,18 @@ app.get('/welcome', function(req, res){
 //==============================
 passport.serializeUser(function(user, done) {
   console.log('serializeUser', user);
-  done (null, user.username);
+  done (null, user.authId);
 });
 
 passport.deserializeUser(function(id, done) {
   console.log('deserializeUser', id);
   for (var i=0;  i<users.length; i++){
     var user = users[i];
-    if (user.username === id) {
+    if (user.authId === id) {
       return done (null, user);
     }
   }
+  done ('There is no user!');
 });
 
 passport.use(new LocalStrategy(
@@ -85,6 +88,33 @@ passport.use(new LocalStrategy(
       }
     ));
 
+passport.use(new FacebookStrategy({
+  clientID: '1115328572241857',
+  clientSecret: '1d1586e167674e393a7d3a9fc0c540d2',
+  callbackURL: "/auth/facebook/callback",
+  profileFields:['id', 'email', 'gender', 'link', 'locale', 
+  'name', 'timezone', 'updated_time', 'verified', 'displayName']
+  },
+  function(accessToken, refreshToken, profile, done) {
+    console.log(profile);
+    var authId = 'facebook:' + profile.id;
+    for (var i=0; i <users.length; i++) {
+      var user = users[i];
+      if (user.authId === authId) {
+        return done(null, user);
+      }
+    }
+    var newuser = { 
+      'authId': authId,
+      'displayName': profile.displayName,
+      'email': profile.emails[0].value
+    };
+    users.push(newuser);
+    done(null,newuser);
+
+  }
+));
+
 app.post(
   '/auth/login',
   passport.authenticate(
@@ -96,9 +126,41 @@ app.post(
     }
   )
 );
+//첫번째 왕복
+app.get(
+  '/auth/facebook', 
+  passport.authenticate(
+    'facebook',
+    {scope: 'email'}
+    )
+);
+//두번째 왕복
+// app.get(
+//   '/auth/facebook/callback',
+//   passport.authenticate(
+//     'facebook', 
+//     { 
+//       successRedirect: '/welcome',
+//       failureRedirect: '/auth/login' 
+//     }
+//     )
+// );
+
+app.get(
+  '/auth/facebook/callback',
+  passport.authenticate(
+    'facebook', 
+    { 
+      failureRedirect: '/auth/login'}),
+  (req, res) => {
+  req.session.save() => {
+  res.redirect('/welcome');
+  }}
+);
 
 var users = [
   {
+    authId: 'loal:egoing',
     username: 'egoing',
     password:'mTi+/qIi9s5ZFRPDxJLY8yAhlLnWTgYZNXfXlQ32e1u/hZePhlq41NkRfffEV+T92TGTlfxEitFZ98QhzofzFHLneWMWiEekxHD1qMrTH1CWY01NbngaAfgfveJPRivhLxLD1iJajwGmYAXhr69VrN2CWkVD+aS1wKbZd94bcaE=',
     salt:'O0iC9xqMBUVl3BdO50+JWkpvVcA5g2VNaYTR5Hc45g+/iXy4PzcCI7GJN5h5r3aLxIhgMN8HSh0DhyqwAp8lLw==',salt:'O0iC9xqMBUVl3BdO50+JWkpvVcA5g2VNaYTR5Hc45g+/iXy4PzcCI7GJN5h5r3aLxIhgMN8HSh0DhyqwAp8lLw==',
@@ -108,6 +170,7 @@ var users = [
 app.post('/auth/register', function(req, res){
   hasher({password: req.body.password}, function(err, pass, salt, hash){
     var user = {
+      authId: 'local:' + req.body.username,
       username: req.body.username,
       password: hash,
       salt: salt,
@@ -155,6 +218,7 @@ app.get('/auth/login', function(req, res){
       <input type="submit">
     </p>
   </form>
+  <a href="/auth/facebook">facebook</a>
   `;
    res.send (output);
 });
