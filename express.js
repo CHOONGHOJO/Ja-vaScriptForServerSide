3ea6acefbeb61bcc71392bfab948228ec4a87cc8
CHOONGHOJO <46162087+CHOONGHOJO@users.noreply.github.com>
2020-11-29 14:31:45 -0500

    Nov/29/231pm
    config folder

---
+++ express.js
@@ -0,0 +1,26 @@
+
+module.exports = function(){
+    var express = require('express');
+    var session = require('express-session');
+    var MySQLStore = require('express-mysql-session')(session);
+    var bodyParser = require('body-parser');
+    
+    var app = express();
+    app.set('views', './views/mysql');
+    app.set('view engine', 'jade');
+    app.use(bodyParser.urlencoded({ extended: false }));
+    app.use(session({
+      secret: 'secret algorithm',
+      resave: false,
+      saveUninitialized: true,
+      store:new MySQLStore({
+        host:'localhost',
+        port:3306,
+        user:'root',
+        password:'123456',
+        database:'o2'
+      })
+    }));
+    
+    return  app;
+}
